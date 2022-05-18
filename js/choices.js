010c5324ad6264082b19b742fb1b2fa1ddca8a7d
So Chigusa <so.chigusa.pp@gmail.com>
2022-05-17 22:39:54 -0700

    json cache, type selection method, id prefix match

--- js/choices.js
+++ js/choices.js
@@ -1,19 +1,19 @@
 
 // identify support card type
 function cardType(name) {
+	// console.log(name);
 	var data = card_type_json.filter(function(item, index){
 		if(item.Cards.indexOf(name) >= 0) return true;
 	});
-	console.log(name);
 	return data[0].Type;
 }
 
 // generate table
-function genTable(json, id, type) {
+function genTable(json, id) {
 
 	// filter the data by input id
 	var data = json.filter(function(item, index){
-  	if((item.ID).indexOf(id) >= 0) return true;
+  	if((item.ID).indexOf(id) == 0) return true;
 	});
 
 	for(var ev of data) {
@@ -25,14 +25,15 @@ function genTable(json, id, type) {
 		document.getElementById('maintable').appendChild(h3);
 
 		// character [character only]
-		if(type == "Character") {
+		var type = ev.Event.Type;
+		if(type == '育成ウマ娘') {
 			var p = document.createElement('p');
 			p.textContent = ev.Event.Character;
 			document.getElementById('maintable').appendChild(p);
 		}
 
 		// card type [support card only]
-		else if(type == "Support Card") {
+		else if(type == 'サポートカード') {
 			for(var ev_type of ev.Event.CardType) {
 				var name = ev.Event.Character+'('+ev_type.Rarity+')'+'［'+ev_type.Name+'］';
 				var p = document.createElement('p');
@@ -43,7 +44,7 @@ function genTable(json, id, type) {
 		}
 
 		// scenario type [main scenario only]
-		else if (type == "Main Scenario") {
+		else if (type == 'メインシナリオ') {
 			var p = document.createElement('p');
 			p.textContent = ev.Event.Scenario;
 			document.getElementById('maintable').appendChild(p);
@@ -102,13 +103,9 @@ function inputChange(event){
 	}
 
 	// table content
-	for(const character_name of ['ain','agnst','agnsd','gor','mzrr','diw','tmm','tie']) {
-		genTable(event_json[character_name], id, "Character");
+	for(const json_name of json_name_list) {
+		genTable(event_json[json_name], id);
 	}
-	for(const card_type of ['spd', 'stm', 'pwr', 'knj', 'ksk']) {
-		genTable(event_json[card_type], id, "Support Card");
-	}
-	genTable(event_json['scenario'], id, "Main Scenario");
 }
 
 // clear textbox on enter key
@@ -121,22 +118,15 @@ function keydown(event) {
 
 // load and save card json database
 let card_type_json, event_json = {};
+var json_name_list = ['ain','agnst','agnsd','gor','mzrr','diw','tmm','tie','spd', 'stm', 'pwr', 'knj', 'ksk','scenario'];
 fetch('https://sochigusa.github.io/Uma-ggrks/json/card_type.json', {cache:'no-store'})
 	.then(response => response.json())
 	.then(json => card_type_json = json);
-for(const character_name of ['ain','agnst','agnsd','gor','mzrr','diw','tmm','tie']) {
-	fetch('https://sochigusa.github.io/Uma-ggrks/json/'+character_name+'.json', {cache:'no-store'})
+for(const json_name of json_name_list) {
+	fetch('https://sochigusa.github.io/Uma-ggrks/json/'+json_name+'.json', {cache:'no-store'})
 		.then(response => response.json())
-		.then(json => event_json[character_name] = json);
+		.then(json => event_json[json_name] = json);
 }
-for(const card_type of ['spd', 'stm', 'pwr', 'knj', 'ksk']) {
-	fetch('https://sochigusa.github.io/Uma-ggrks/json/'+card_type+'.json', {cache:'no-store'})
-		.then(response => response.json())
-		.then(json => event_json[card_type] = json);
-}
-fetch('https://sochigusa.github.io/Uma-ggrks/json/scenario.json', {cache:'no-store'})
-	.then(response => response.json())
-	.then(json => event_json['scenario'] = json);
 
 // update on input
 let text = document.getElementById('idText');
