4411757a9dd140e539adcb466695d833002fec5b
So Chigusa <so.chigusa.pp@gmail.com>
2022-05-20 18:19:29 -0700

    UI / css improved

--- js/choices.js
+++ js/choices.js
@@ -11,49 +11,63 @@ function cardType(name) {
 // generate table
 function genTable(json, id) {
 
-	// filter the data by input id
-	var data = json.filter(function(item, index){
-  	if((item.ID).indexOf(id) == 0) return true;
-	});
+	// show only if id_length >= 3 or is_show_all
+	var data = [];
+	let is_show_all = document.getElementById('is_show_all');
+	if(id.length >= 3 || is_show_all.checked) {
+		// filter the data by input id
+		var data = json.filter(function(item, index){
+	  	if((item.ID).indexOf(id) == 0) return true;
+		});
+	}
 
 	for(var ev of data) {
 		// console.log(ev);
 
+		// div block and table for an event
+		var event_div = document.createElement('div');
+		var table = document.createElement('table');
+
 		// event title
 		var h3 = document.createElement('h3');
 		h3.textContent = '['+ev.ID+'] '+ev.Title;
-		document.getElementById('maintable').appendChild(h3);
+		event_div.appendChild(h3);
 
 		// character [character only]
 		var type = ev.Event.Type;
 		if(type == '育成ウマ娘') {
+			table.classList.add("character");
 			for (var chara of ev.Event.Character) {
 				var p = document.createElement('p');
 				p.textContent = chara;
-				document.getElementById('maintable').appendChild(p);
+				p.classList.add('source');
+				event_div.appendChild(p);
 			}
 		}
 
 		// card type [support card only]
 		else if(type == 'サポートカード') {
+			table.classList.add('support');
 			for(var ev_type of ev.Event.CardType) {
 				var name = ev.Event.Character+'('+ev_type.Rarity+')'+'［'+ev_type.Name+'］';
 				var p = document.createElement('p');
 				p.textContent = name;
 				p.classList.add(cardType(name));
-				document.getElementById('maintable').appendChild(p);
+				p.classList.add('source');
+				event_div.appendChild(p);
 			}
 		}
 
 		// scenario type [main scenario only]
 		else if (type == 'メインシナリオ') {
+			table.classList.add("scenario");
 			var p = document.createElement('p');
 			p.textContent = ev.Event.Scenario;
-			document.getElementById('maintable').appendChild(p);
+			p.classList.add('source');
+			event_div.appendChild(p);
 		}
 
 		// make table
-		var table = document.createElement('table');
 		var tr = document.createElement('tr');
 		var th = document.createElement('th');
 		th.textContent = '選択肢';
@@ -89,7 +103,9 @@ function genTable(json, id) {
 			table.appendChild(tr);
 		}
 
-		maintable.appendChild(table);
+		// append
+		event_div.appendChild(table);
+		document.getElementById('maintable').appendChild(event_div);
 	}
 
 }
@@ -113,8 +129,11 @@ function inputChange(event){
 // clear textbox on enter key
 function keydown(event) {
 	if(event.key == 'Enter') {
-		document.getElementById('idText').value = '';
-		inputChange(event);
+		let is_clear_by_enter = document.getElementById('is_clear_by_enter');
+		if(is_clear_by_enter.checked) {
+			document.getElementById('idText').value = '';
+			inputChange(event);
+		}
 	}
 }
 
@@ -132,5 +151,10 @@ for(const json_name of json_name_list) {
 
 // update on input
 let text = document.getElementById('idText');
+let is_show_all = document.getElementById('is_show_all');
 text.addEventListener('input', inputChange);
 text.addEventListener('keydown', keydown);
+is_show_all.addEventListener('change', event => {
+	let input_event = new Event('input');
+	text.dispatchEvent(input_event);
+});
