db6fb9f0476c586c94aa12306ca3e6a0eb255c8b
So Chigusa <chigusa@hep-th.phys.s.u-tokyo.ac.jp>
2022-06-17 17:58:31 -0700

    inr / new eis, siu, kng, krn

--- js/choices.js
+++ js/choices.js
@@ -2,8 +2,8 @@
 // identify support card type
 function cardType(name) {
 	// console.log(name);
-	var data = card_type_json.filter(function(item, index){
-		if(item.Cards.indexOf(name) >= 0) return true;
+	var data = card_type_json.filter(function (item, index) {
+		if (item.Cards.indexOf(name) >= 0) return true;
 	});
 	return data[0].Type;
 }
@@ -14,14 +14,14 @@ function genTable(json, id) {
 	// show only if id_length >= 3 or is_show_all
 	var data = [];
 	let is_show_all = document.getElementById('is_show_all');
-	if(id.length >= 3 || is_show_all.checked) {
+	if (id.length >= 3 || is_show_all.checked) {
 		// filter the data by input id
-		var data = json.filter(function(item, index){
-	  	if((item.ID).indexOf(id) == 0) return true;
+		var data = json.filter(function (item, index) {
+			if ((item.ID).indexOf(id) == 0) return true;
 		});
 	}
 
-	for(var ev of data) {
+	for (var ev of data) {
 		// console.log(ev);
 
 		// div block and table for an event
@@ -30,12 +30,12 @@ function genTable(json, id) {
 
 		// event title
 		var h3 = document.createElement('h3');
-		h3.textContent = '['+ev.ID+'] '+ev.Title;
+		h3.textContent = '[' + ev.ID + '] ' + ev.Title;
 		event_div.appendChild(h3);
 
 		// character [character only]
 		var type = ev.Event.Type;
-		if(type == '育成ウマ娘') {
+		if (type == '育成ウマ娘') {
 			table.classList.add("character");
 			for (var chara of ev.Event.Character) {
 				var p = document.createElement('p');
@@ -46,10 +46,10 @@ function genTable(json, id) {
 		}
 
 		// card type [support card only]
-		else if(type == 'サポートカード') {
+		else if (type == 'サポートカード') {
 			table.classList.add('support');
-			for(var ev_type of ev.Event.CardType) {
-				var name = ev.Event.Character+'('+ev_type.Rarity+')'+'［'+ev_type.Name+'］';
+			for (var ev_type of ev.Event.CardType) {
+				var name = ev.Event.Character + '(' + ev_type.Rarity + ')' + '［' + ev_type.Name + '］';
 				var p = document.createElement('p');
 				p.textContent = name;
 				p.classList.add(cardType(name));
@@ -80,7 +80,7 @@ function genTable(json, id) {
 		table.appendChild(tr);
 
 		// list of choices and results
-		for(var chs of ev.Choices) {
+		for (var chs of ev.Choices) {
 			var tr = document.createElement('tr');
 			var td = document.createElement('td');
 			td.textContent = chs.Choice;
@@ -88,12 +88,12 @@ function genTable(json, id) {
 			var td = document.createElement('td');
 
 			// Success / Fail-type events
-			if(chs.Results instanceof Array) {
+			if (chs.Results instanceof Array) {
 				td.innerHTML = chs.Results.join('<br>');
 			} else {
 				tdelem = "";
-				for(key in chs.Results) {
-					tdelem += '<br><b>'+key+'</b><br>';
+				for (key in chs.Results) {
+					tdelem += '<br><b>' + key + '</b><br>';
 					tdelem += chs.Results[key].join('<br>')
 				}
 				td.innerHTML = tdelem.slice(4);
@@ -111,26 +111,26 @@ function genTable(json, id) {
 }
 
 // update table on input
-function inputChange(event){
-  id = event.currentTarget.value;
+function inputChange(event) {
+	id = event.currentTarget.value;
 
 	// remove previous table (if any)
 	maintable = document.getElementById('maintable');
-	while(maintable.firstChild){
+	while (maintable.firstChild) {
 		maintable.removeChild(maintable.firstChild);
 	}
 
 	// table content
-	for(const json_name of json_name_list) {
+	for (const json_name of json_name_list) {
 		genTable(event_json[json_name], id);
 	}
 }
 
 // clear textbox on enter key
 function keydown(event) {
-	if(event.key == 'Enter') {
+	if (event.key == 'Enter') {
 		let is_clear_by_enter = document.getElementById('is_clear_by_enter');
-		if(is_clear_by_enter.checked) {
+		if (is_clear_by_enter.checked) {
 			document.getElementById('idText').value = '';
 			inputChange(event);
 		}
@@ -139,12 +139,12 @@ function keydown(event) {
 
 // load and save card json database
 let card_type_json, event_json = {};
-var json_name_list = ['spd','stm','pwr','knj','ksk','scenario'].concat( ['ain','agnst','agnsd','adm','wnn','wk','eag','eis','erk','ogr','krn','kwk','kts','kng','grs','gorst','gorsp','sir','skrt','skrb','stn','snb','sup','sps','sma','siu','tik','diw','tmm','tie','tuk','tos','nis','nrtt','nrtb','nsn','hru','hsak','hsam','bwh','fin','hzk','mtknt','mtknh','myn','mrz','mnh','mhn','mis','mzra','mzrd','mzrp','mzrb','mzrm','mzrr','yen','ris']);
-fetch('https://sochigusa.github.io/Uma-ggrks/json/card_type.json', {cache:'no-store'})
+var json_name_list = ['spd', 'stm', 'pwr', 'knj', 'ksk', 'scenario'].concat(['ain', 'agnst', 'agnsd', 'adm', 'inr', 'wnn', 'wk', 'eag', 'eis', 'erk', 'ogr', 'krn', 'kwk', 'kts', 'kng', 'grs', 'gorst', 'gorsp', 'sir', 'skrt', 'skrb', 'stn', 'snb', 'sup', 'sps', 'sma', 'siu', 'tik', 'diw', 'tmm', 'tie', 'tuk', 'tos', 'nis', 'nrtt', 'nrtb', 'nsn', 'hru', 'hsak', 'hsam', 'bwh', 'fin', 'hzk', 'mtknt', 'mtknh', 'myn', 'mrz', 'mnh', 'mhn', 'mis', 'mzra', 'mzrd', 'mzrp', 'mzrb', 'mzrm', 'mzrr', 'yen', 'ris']);
+fetch('https://sochigusa.github.io/Uma-ggrks/json/card_type.json', { cache: 'no-store' })
 	.then(response => response.json())
 	.then(json => card_type_json = json);
-for(const json_name of json_name_list) {
-	fetch('https://sochigusa.github.io/Uma-ggrks/json/'+json_name+'.json', {cache:'no-store'})
+for (const json_name of json_name_list) {
+	fetch('https://sochigusa.github.io/Uma-ggrks/json/' + json_name + '.json', { cache: 'no-store' })
 		.then(response => response.json())
 		.then(json => event_json[json_name] = json);
 }
--- src/to_json.py
+++ src/to_json.py
@@ -1,22 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import collections
 import re
 import json
 from bs4 import BeautifulSoup
 
 # convert parse results into dictionary
+
+
 def to_dictionary(title, id, charas, choices, results, type):
-    chdict = [] # Deform results structure
+    chdict = []  # Deform results structure
     for choice, result in zip(choices, results):
-        header = result.select('.umamusume-event-checker__choice--result-header')
-        if(header != []): # success/failure-type evevnts
-            casedict = {};
+        header = result.select(
+            '.umamusume-event-checker__choice--result-header')
+        if(header != []):  # success/failure-type evevnts
+            casedict = {}
             for result_caseA in result.find_all('div', recursive=False):
-                header = result_caseA.select('.umamusume-event-checker__choice--result-header')[0].text
-                reslist_caseA = [res.text.removeprefix('・') for res in result_caseA.find_all('div')]
+                header = result_caseA.select(
+                    '.umamusume-event-checker__choice--result-header')[0].text
+                reslist_caseA = [res.text.removeprefix(
+                    '・') for res in result_caseA.find_all('div')]
                 casedict[header] = reslist_caseA
             chdict.append(dict([("Choice", choice), ("Results", casedict)]))
         else:
-            reslist = [res.text.removeprefix('・') for res in result.find_all('div')]
+            reslist = [res.text.removeprefix('・')
+                       for res in result.find_all('div')]
             chdict.append(dict([("Choice", choice), ("Results", reslist)]))
 
     event = {}
@@ -24,19 +32,24 @@ def to_dictionary(title, id, charas, choices, results, type):
         event = dict([("Type", "育成ウマ娘"), ("Character", charas)])
     elif(type["Type"] == "Support Card"):
         chdat = []
-        typedict = [] # deform characters structure
+        typedict = []  # deform characters structure
         for chara in charas:
             chdat = re.split('[\(\)]', chara)
-            typedict.append(dict([("Rarity", chdat[1]), ("Name", chdat[2].strip('［］'))]))
-        event = dict([("Type", "サポートカード"), ("Character", chdat[0]), ("CardType", typedict)])
+            typedict.append(
+                dict([("Rarity", chdat[1]), ("Name", chdat[2].strip('［］'))]))
+        event = dict([("Type", "サポートカード"), ("Character",
+                     chdat[0]), ("CardType", typedict)])
     elif(type["Type"] == "Main Scenario"):
         chdat = re.split('[［]', charas[0])
         event = dict([("Type", "メインシナリオ"), ("Scenario", chdat[0])])
 
-    mydict = dict([("Title", title), ("ID", id), ("Event", event), ("Choices", chdict)])
+    mydict = dict([("Title", title), ("ID", id),
+                  ("Event", event), ("Choices", chdict)])
     return mydict
 
 # convert html to json
+
+
 def html_to_json(src_html, src_json, type):
 
     # open html file
@@ -49,14 +62,19 @@ def html_to_json(src_html, src_json, type):
     for card in soup.body.find_all('div', recursive=False):
         for event in card.select('.umamusume-event-checker__event'):
             title = event.select('.umamusume-event-checker__event--title')
-            if(title != []): # omit .umamusume-event-checker__event no-event
+            if(title != []):  # omit .umamusume-event-checker__event no-event
                 title = title[0].text
-                id = event.select('.umamusume-event-checker__event--id')[0].text
-                charas = event.select('.umamusume-event-checker__event--character')[0]
+                id = event.select(
+                    '.umamusume-event-checker__event--id')[0].text
+                charas = event.select(
+                    '.umamusume-event-checker__event--character')[0]
                 charas = [chara.text for chara in charas.find_all('a')]
-                choices = [ch.select('.umamusume-event-checker__choice--body')[0].text for ch in event.find_all('tr')]
-                results = [ch.select('.umamusume-event-checker__choice--result')[0] for ch in event.find_all('tr')]
-                card_dict.append(to_dictionary(title, id, charas, choices, results, type))
+                choices = [ch.select(
+                    '.umamusume-event-checker__choice--body')[0].text for ch in event.find_all('tr')]
+                results = [ch.select(
+                    '.umamusume-event-checker__choice--result')[0] for ch in event.find_all('tr')]
+                card_dict.append(to_dictionary(
+                    title, id, charas, choices, results, type))
 
     # save as json
     with open(src_json, 'w') as f:
@@ -65,6 +83,8 @@ def html_to_json(src_html, src_json, type):
     return card_dict
 
 # check ID duplication with prefix match
+
+
 def duplication_check(card_dict):
     for card in card_dict:
         for card2 in card_dict:
@@ -72,19 +92,23 @@ def duplication_check(card_dict):
                 print(card)
                 print(card2)
 
+
 # preserve whole dictionary for duplication check
 card_dict_all = []
 
 # character events
-for character in ['ain','agnst','agnsd','adm','wnn','wk','eag','eis','erk','ogr','krn','kwk','kts','kng','grs','gorst','gorsp','sir','skrt','skrb','stn','snb','sup','sps','sma','siu','tik','diw','tmm','tie','tuk','tos','nis','nrtt','nrtb','nsn','hru','hsak','hsam','bwh','fin','hzk','mtknt','mtknh','myn','mrz','mnh','mhn','mis','mzra','mzrd','mzrp','mzrb','mzrm','mzrr','yen','ris']:
-    card_dict_all += html_to_json('html/iks/'+character+'.html', 'json/'+character+'.json', {"Type": "Character"})
+for character in ['ain', 'agnst', 'agnsd', 'adm', 'inr', 'wnn', 'wk', 'eag', 'eis', 'erk', 'ogr', 'krn', 'kwk', 'kts', 'kng', 'grs', 'gorst', 'gorsp', 'sir', 'skrt', 'skrb', 'stn', 'snb', 'sup', 'sps', 'sma', 'siu', 'tik', 'diw', 'tmm', 'tie', 'tuk', 'tos', 'nis', 'nrtt', 'nrtb', 'nsn', 'hru', 'hsak', 'hsam', 'bwh', 'fin', 'hzk', 'mtknt', 'mtknh', 'myn', 'mrz', 'mnh', 'mhn', 'mis', 'mzra', 'mzrd', 'mzrp', 'mzrb', 'mzrm', 'mzrr', 'yen', 'ris']:
+    card_dict_all += html_to_json('html/iks/'+character+'.html',
+                                  'json/'+character+'.json', {"Type": "Character"})
 
 # support card events
 for card_type in ['spd', 'stm', 'pwr', 'knj', 'ksk']:
-    card_dict_all += html_to_json('html/spt/'+card_type+'.html', 'json/'+card_type+'.json', {"Type": "Support Card"})
+    card_dict_all += html_to_json('html/spt/'+card_type+'.html',
+                                  'json/'+card_type+'.json', {"Type": "Support Card"})
 
 # main scenario events
-card_dict_all += html_to_json('html/scenario.html', 'json/scenario.json', {"Type": "Main Scenario"})
+card_dict_all += html_to_json('html/scenario.html',
+                              'json/scenario.json', {"Type": "Main Scenario"})
 
 # duplication check of IDs
 duplication_check(card_dict_all)
